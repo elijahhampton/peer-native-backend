78ed5f160e9510ab861d5207488082cad30145a1
Elijah Hampton <elijahhampton@MBP-de-Elijah-2.lan>
2023-04-10 23:53:44 -0400

    Complets api routes for greet and reply.

--- index.ts
+++ index.ts
@@ -1,44 +1,46 @@
 require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser');
 const fs = require('fs')
+
+const apiKey = process.env.OPEN_AI_API_KEY
+
 
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  apiKey: process.env.OPEN_AI_API_KEY
+  apiKey
 });
-
 
 const openai = new OpenAIApi(configuration);
 
-const express = require('express')
 const app = express()
+app.use(express.json())
 
-app.get('/', async (request, response) => {
-
-  const resp = await openai.createTranscription(
-    fs.createReadStream("french_girl.mp3"),
-    "whisper-1"
-  );
-
-  console.log("VIDEO TEXT: ")
-  console.log(resp.data)
-
-  const PROMPT = "You are a 26 year old male from New York, New York and you are an expert in the English language including the Bronx accent. You should analyze any text received as if the speaker is a B2 level according to the Common European Framework of Reference for Languages. Can you provide a list of word replacements that the speaker can use to sound more like she was born in the Bronx in New York? Provide this in the form of a list with reasons why the word replacements sound more like someone born in the Bronx? Can you analyze the use of the language and suggest how it can adapted to sound more formal? (Provide this in the form of a list with reasons why the suggestion sounds more formal."
+app.post('/greeting', async (request, response) => {
+  const PROMPT = process.env.PROMPT + ` "${request.body.greeting}"`
 
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
       { role: "system", content: `${PROMPT}` },
-      { role: "system", content: `${resp.data}` }
+    ],
+  })
+
+  response.status(200).send({ greetingResponse: completion.data.choices[0].message, originalPrompt: PROMPT })
+})
+
+app.post('/reply', async (request, response) => {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      ...request.body.pastConversation,
+      { role: "user", content: `This is the next reply: "${request.body.userResponse}"` },
     ],
   });
 
-  console.log('ANALISIS')
-  console.log(completion.data.choices[0].message);
-
-  response.json(completion.data.choices[0].message)
-
-
-
+  response.status(200).send({
+    replyResponse: completion.data.choices[0].message,
+  })
 })
 
 const PORT = 3001
---
+++ test.ts
@@ -0,0 +1,98 @@
+//@ts-nocheck
+const axios = require('axios')
+
+const TEST_ENDPOINT = 'http://localhost:3001'
+const API_GREETING = `${TEST_ENDPOINT}/greeting`
+const REPLY_GREETING = `${TEST_ENDPOINT}/reply`
+
+const commonAxiosConfig = {
+    method: "POST",
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+}
+
+const userResponses = [
+    "Actually, I want to help improve politics.",
+    "Thank you for speaking with me about politics. What do you think about the democrats?",
+    "Okay. What do you think about local government?"
+]
+
+interface IGPTResponse {
+    suggestion: string;
+    response: string;
+    error?: string;
+}
+
+//@ts-ignore
+const conversation = []
+const chatGPTConversationCache = []
+
+const PROMPT = process.env.PROMPT + ` "Politics have largely destroyed this country".`
+async function main() {
+    chatGPTConversationCache.push({ role: 'system', content: PROMPT })
+
+    //send greeting
+    axios(`${API_GREETING}`, {
+        ...commonAxiosConfig,
+        data: JSON.stringify({ greeting: "Politics have largely destroyed this country." + "\"" }),
+    }).then(async (axiosResponse) => {
+        const { greetingResponse: { role, content }, originalPrompt } = axiosResponse.data
+
+        const parsedContent = JSON.parse(content)[0]
+        const { response, suggestion } = parsedContent
+
+        let error = ""
+        if (parsedContent?.error) {
+            error = parsedContent.error
+        }
+
+        //@ts-ignore
+        conversation.push({ role, response, suggestion, error })
+        chatGPTConversationCache.push({ role: 'assistant', content })
+
+
+        for (let i = 0; i < 3; i++) {
+            const userResponse = userResponses[i]
+            const axiosResponse = await axios(`${REPLY_GREETING}`, {
+                ...commonAxiosConfig,
+                data: JSON.stringify({
+                    userResponse,
+                    pastConversation: chatGPTConversationCache
+                })
+            })
+
+            //thinking...
+
+            //process reply
+            const replyResponse = axiosResponse.data.replyResponse
+            console.log(replyResponse.content)
+            const newContent = String(replyResponse.content).replace(".", "");
+            const newRole = replyResponse.role
+
+            //add old convo cache
+            chatGPTConversationCache.push({ role: 'user', content: userResponse })
+            chatGPTConversationCache.push({ role: newRole, content: newContent })
+
+            let newError = ""
+            if (newContent[0]?.error) {
+                newError = newContent[0].error
+            }
+
+            //add to convo
+            conversation.push({ role: 'user', response: userResponse })
+            conversation.push({ role: 'system', response: JSON.parse(newContent)[0]?.response, suggestion: JSON.parse(newContent)[0]?.suggestion, error: newError })
+
+
+        }
+
+        console.log('Conversation Array: ')
+        console.log(conversation)
+
+        console.log('Conversation Cache: ')
+        console.log(chatGPTConversationCache)
+    })
+}
+
+main()
