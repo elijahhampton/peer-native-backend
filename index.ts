d1b992bb25d6856de6b36845a28a9a7cd5ae1175
Elijah Hampton <elijahhampton@MacBook-Pro-de-Elijah-2.local>
2023-04-22 12:03:55 -0400

    Adds catch all route before other implemented routes to prevent app crash in heroku.

--- index.ts
+++ index.ts
@@ -15,6 +15,12 @@ const openai = new OpenAIApi(configuration);
 const app = express()
 app.use(express.json())
 
+app.use((req, res, next) => {
+  console.log('Caught here... working')
+  res.status(404).send('Sorry, the route you are trying to access does not exist.');
+});
+
+
 app.post('/greeting', async (request, response) => {
   const PROMPT = `ChatGPT your only goal is to have a conversation with me about ${request.body.topic} in ${request.body.user_target_language}. Please stay on the topic of ${request.body.topic} for the duration of this conversation. 
   If I bring up a topic that is not related to ${request.body.topic}, please gently steer the conversation back to the topic at hand. You should speak with me in a natural way as if I am a friend. Also, I would like you to analyze my responses as a ${request.body.training_level} ${request.body.user_target_language} speaker. 
@@ -46,12 +52,6 @@ app.post('/reply', async (request, response) => {
     timestamp: new Date()
   })
 })
-
-app.use((req, res, next) => {
-  console.log('Caught here... working')
-  res.status(404).send('Sorry, the route you are trying to access does not exist.');
-});
-
 
 const PORT = 3001
 app.listen(PORT, () => {
